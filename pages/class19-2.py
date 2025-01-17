1c5ed2fa0f42d853c203efa420b0fce31a9b4030
Charles-Monster <charlesweng1011@gmail.com>
2025-01-17 19:53:30 +0800

    TEST

--- pages/class19-2.py
+++ pages/class19-2.py
@@ -1,21 +1,31 @@
+
 import streamlit as st
 from utils import init_page
-
-
 init_page()
-st.title("聊天室")
-
-if "meassges" not in st.session_state:
-    st.session_state.messages=[]
-
+st.title("這是標題")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-if message:=st.chat_input("請輸入訊息"):
+if message := st.chat_input("請輸入你的問題"):
     with st.chat_message("user"):
         st.write(message)
-    st.session_state.messages.append({"role":"user","content":message})
-    assistant_response=f"你剛才說了:{message}"
+    st.session_state.messages.append({"role": "user", "content": message})
+    assistant_response = f"你剛剛說了：{message}"
     with st.chat_message("assistant"):
         st.write(assistant_response)
-    st.session_state.messages.append({"role":"assistant","content":assistant_response})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": assistant_response}
+    )
+
+
+
+
+
+
+
+
+
+
+
